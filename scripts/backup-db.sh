26631fb7c9799be1eaacddc2be50f46ea0cf0cc7
briced <delanoe.brice@live.fr>
2021-10-26 09:29:20 +0200

    backup end

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -1,4 +1,4 @@
-DBNAME  = tp1 # nom de la base de données à sauvegarder
-TIME    = $(date '+%Y-%m-%d_%H-%M-%S') 
+DBNAME=tp1 # nom de la base de données à sauvegarder
+TIME=$(date '+%Y-%m-%d_%H-%M-%S') # horodatage
 
 mysqldump $DBNAME -uroot -p$MYSQL_ROOT_PASSWORD > dump/$DBNAME.$TIME.sql
